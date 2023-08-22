81b40963c04f7a9a84d89ea2581fa310dc13e43e
Cagri Gider <cagri.gider@adesso.com.tr>
2023-08-22 09:36:34 +0300

    Initial Commit

---
+++ Map-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Map-UIKit
+//
+//  Created by Cagri Gider on 22.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
